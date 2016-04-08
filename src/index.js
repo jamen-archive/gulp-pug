af88b707420f95a5fc229280b0cf44a19c633d0a
Jacob Parker <jacobparker1992@gmail.com>
2016-04-08 14:37:37 +0100

    Allow imports

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import through from 'through2';
 import pug from 'pug';
 import { join, basename } from 'path';
 import { PluginError, replaceExtension } from 'gulp-util';
+import { _extend as extend } = 'util';
 
-export default function gulpPug(opts = {}) {
+export default function gulpPug(baseOpts = {}) {
   return through.obj(function stream(file, enc, callback) {
     let error = null;
 
     if (file.isBuffer()) {
+      const opts = extend({ filename: file.path }, baseOpts);
       const contents = pug.render(file.contents.toString(), opts);
       file.contents = new Buffer(contents);
     } else {
